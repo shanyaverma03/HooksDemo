7ca9297fb3d4ff6cb5ce07024d0a8f3d607c1c49
Shanya <shanyaverma03@gmail.com>
2023-12-05 01:21:13 +0530

    Change useState example

--- src/components/RefsDemo.js
+++ src/components/RefsDemo.js
@@ -4,14 +4,33 @@ import classes from "./RefsDemo.module.css";
 
 const RefsDemo = () => {
   const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
   const nameChangeHandler = (event) => {
     setName(event.target.value);
   };
+
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const submitHandler = () => {
+    console.log("Welcome", name);
+    setName("");
+    setPassword("");
+  };
   return (
     <div className={classes.wrapper}>
-      <h1>Hello {name}</h1>
+      <h1>Please enter details</h1>
+      <label>Name</label>
       <input type="text" onChange={nameChangeHandler} value={name} />
+      <label>Password</label>
+      <input
+        type="password"
+        onChange={passwordChangeHandler}
+        value={password}
+      />
+      <button onClick={submitHandler}>Submit</button>
     </div>
   );
 };
