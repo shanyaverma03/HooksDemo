99ce1529d663087dc0755c63f5f79bbbf8cd1fcf
Shanya <shanyaverma03@gmail.com>
2023-12-06 03:00:06 +0530

    Add cleanup function in useeffect

--- src/components/EffectDemo/UseEffectDemo.js
+++ src/components/EffectDemo/UseEffectDemo.js
@@ -8,13 +8,16 @@ const UseEffectDemo = () => {
   useEffect(() => {
     getUsers(option);
     console.log("in use effect");
+
+    return () => {
+      console.log("Cleaning up");
+    };
   }, [option]);
 
   const getUsers = async (option) => {
     const url = `https://jsonplaceholder.typicode.com/${option}/1`;
     const response = await fetch(url);
     const responseData = await response.json();
-
     console.log(responseData);
   };
 
